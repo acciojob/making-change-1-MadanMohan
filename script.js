860610d669d996b8e3ddd37c6020e9d3063a7c71
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-07-22 18:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const makeChange = (c) => {
-  // your name here
+  let ans = {
+    q: 0,
+    d: 0,
+    n: 0,
+    p: 0,
+  };
+
+  let denominations = [
+    ['q', 25],
+    ['d', 10],
+    ['n', 5],
+    ['p', 1],
+  ];
+
+  for (let i = 0; i < denominations.length; i++) {
+    let [symbol, value] = denominations[i];
+    while (c >= value) {
+      c -= value;
+      ans[symbol]++;
+    }
+  }
+
+  return ans;
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+alert(JSON.stringify(makeChange(Number(c))));
